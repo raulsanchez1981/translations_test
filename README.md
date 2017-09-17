3b9f3b7c78c3c41fe9e74ae8f08ea625d5a47659
Raul Sanchez <raulsanchez1981@gmail.com>
2017-09-17 13:48:07 +0200

    Minor Changes

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# translations_test
-Challenge for Translations.com
+Challenge for Transperfect (BackEnd)
+
+The challenge create a REST API for managing imaginary Issues.
+
+Tools and Frameworks I used:
+
+- Spring Boot 1.5.7
+- Spring Data Mongo
+- MongoDB Embedded
+- Swagger UI
+- Cucumber for Functional Test
+- Junit and Mockito for Unitary Test
+
+All the code are in GitHub and exist an image of this in DockerHub*.
+
+
--- target/cucumber-html-report/report.js
+++ target/cucumber-html-report/report.js
@@ -8,9 +8,9 @@ formatter.feature({
 });
 formatter.scenario({
   "line": 5,
-  "name": "SC01. Check the search request retrieve all issues from the database.",
+  "name": "SC01. Check the search request retrieve the correct number of issues from the database.",
   "description": "",
-  "id": "we-want-to-test-the-all-issues-searching-service-for-the-project;sc01.-check-the-search-request-retrieve-all-issues-from-the-database.",
+  "id": "we-want-to-test-the-all-issues-searching-service-for-the-project;sc01.-check-the-search-request-retrieve-the-correct-number-of-issues-from-the-database.",
   "type": "scenario",
   "keyword": "Scenario"
 });
@@ -52,30 +52,7 @@ formatter.step({
 });
 formatter.step({
   "line": 11,
-  "name": "it should be returned this list of issues with:",
-  "rows": [
-    {
-      "cells": [
-        "title",
-        "locked"
-      ],
-      "line": 12
-    },
-    {
-      "cells": [
-        "Issue 1",
-        "false"
-      ],
-      "line": 13
-    },
-    {
-      "cells": [
-        "Issue 2",
-        "false"
-      ],
-      "line": 14
-    }
-  ],
+  "name": "it should be returned this list of issues with \u00272\u0027 elements",
   "keyword": "Then "
 });
 formatter.match({
@@ -88,20 +65,191 @@ formatter.match({
   "location": "IssueSearchSteps.createNewIssues(String,Issue\u003e)"
 });
 formatter.result({
-  "duration": 298073435,
-  "error_message": "java.lang.NullPointerException\r\n\tat issues.IssueSearchSteps.createNewIssues(IssueSearchSteps.java:30)\r\n\tat ✽.Given the next issues already created in the system for the owner \u0027test\u0027(issues/features/searchIssues.feature:6)\r\n",
-  "status": "failed"
+  "duration": 563814975,
+  "status": "passed"
 });
 formatter.match({
   "location": "IssueSearchSteps.searchIssues()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 84848820,
+  "status": "passed"
 });
 formatter.match({
-  "location": "IssueSearchSteps.checkFoundIssues(Issue\u003e)"
+  "arguments": [
+    {
+      "val": "2",
+      "offset": 48
+    }
+  ],
+  "location": "IssueSearchSteps.countFoundIssues(long)"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 364308,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 10924718,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 13,
+  "name": "SC02. Check the search request retrieve all the issues with unique id from the database.",
+  "description": "",
+  "id": "we-want-to-test-the-all-issues-searching-service-for-the-project;sc02.-check-the-search-request-retrieve-all-the-issues-with-unique-id-from-the-database.",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 14,
+  "name": "the next issues already created in the system for the owner \u0027test\u0027",
+  "rows": [
+    {
+      "cells": [
+        "state",
+        "title",
+        "url"
+      ],
+      "line": 15
+    },
+    {
+      "cells": [
+        "open",
+        "Issue 1",
+        "www.test.com"
+      ],
+      "line": 16
+    },
+    {
+      "cells": [
+        "closed",
+        "Issue 2",
+        "www.google.com"
+      ],
+      "line": 17
+    }
+  ],
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 18,
+  "name": "the user requests all the issues in the system",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 19,
+  "name": "it should be returned this list of issues with not empty id",
+  "keyword": "Then "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "test",
+      "offset": 61
+    }
+  ],
+  "location": "IssueSearchSteps.createNewIssues(String,Issue\u003e)"
+});
+formatter.result({
+  "duration": 5312411,
+  "status": "passed"
+});
+formatter.match({
+  "location": "IssueSearchSteps.searchIssues()"
+});
+formatter.result({
+  "duration": 2560000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "IssueSearchSteps.checkIdFoundIssues()"
+});
+formatter.result({
+  "duration": 17675898,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 2354871,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 21,
+  "name": "SC03. Check the search request retrieve all issues from the database.",
+  "description": "",
+  "id": "we-want-to-test-the-all-issues-searching-service-for-the-project;sc03.-check-the-search-request-retrieve-all-issues-from-the-database.",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 22,
+  "name": "the next issues already created in the system for the owner \u0027test\u0027",
+  "rows": [
+    {
+      "cells": [
+        "state",
+        "title",
+        "url"
+      ],
+      "line": 23
+    },
+    {
+      "cells": [
+        "open",
+        "Issue 1",
+        "www.test.com"
+      ],
+      "line": 24
+    },
+    {
+      "cells": [
+        "closed",
+        "Issue 2",
+        "www.google.com"
+      ],
+      "line": 25
+    }
+  ],
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 26,
+  "name": "the user requests all the issues in the system",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 27,
+  "name": "it should be returned this list of issues with all the elemets unlocked",
+  "keyword": "Then "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "test",
+      "offset": 61
+    }
+  ],
+  "location": "IssueSearchSteps.createNewIssues(String,Issue\u003e)"
+});
+formatter.result({
+  "duration": 5464615,
+  "status": "passed"
+});
+formatter.match({
+  "location": "IssueSearchSteps.searchIssues()"
+});
+formatter.result({
+  "duration": 2674462,
+  "status": "passed"
+});
+formatter.match({
+  "location": "IssueSearchSteps.checkFoundIssues()"
+});
+formatter.result({
+  "duration": 577641,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 1799384,
+  "status": "passed"
 });
 });
